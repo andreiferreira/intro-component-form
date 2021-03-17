c167e35fe8a30a9e05875387e990182c2ca62ecd
andreiferreira <andreiferreira@outlook.com>
2021-03-17 12:30:06 -0300

    project finished

--- src/js/script.js
+++ src/js/script.js
@@ -4,31 +4,43 @@ var email = document.querySelector("#input_email");
 var password = document.querySelector("#input_password");
 var form = document.querySelector(".section-form__form___inputs");
 var inputs = document.querySelectorAll(".section-form__form___inputs____single-input");
-var mensagemErro = document.querySelector(".section-form__form__text-error");
-
+var erroMessage = document.querySelector(".section-form__form__text-error");
 var countErrors = 0;
 
-function validaForm(event){
-
-    if(firstName.value === "")mudaInput(firstName);
-    if(lastName.value === "") mudaInput(lastName);
-    if(email.value === "") mudaInput(email);
-    if(password.value === "") mudaInput(password);
-
-    if((countErrors > 0) && (mensagemErro === undefined)){
-        mensagemErro.innerText = "Por favor, preencha todos os campos";
-        mensagemErro.classList.add("mensagem-erro")
-    }
-    inputs.forEach(input => input.addEventListener('click', function(){
-        input.style.borderColor = "black";
-    
-    }))
-    
-
+// validates form
+function validateForm(event) {
     event.preventDefault();
+
+    countErrors = 0;
+
+    if (firstName.value === "") changeInput(firstName);
+    if (lastName.value === "") changeInput(lastName);
+    if (email.value === "") changeInput(email);
+    if (password.value === "") changeInput(password);
+
+    // there are errors (entries not filled in) and an error message has not been created
+    if ((countErrors > 0) && (erroMessage.textContent === "")) {
+
+        erroMessage.innerHTML = "Please fill in all the required fields.";
+        erroMessage.classList.add("message-error")
+
+    // if there are no erros (all the entries filled in)
+    } if (countErrors === 0) {
+        Swal.fire("Your data has been sent!")
+        window.setTimeout(function () {
+            form.submit();
+        }, 2000)
+    }
+
+    //checks if the field has been clicked (logically to be filled) by returning the border to its original color
+    inputs.forEach(input => input.addEventListener('click', function () {
+        input.style.borderColor = "black";
+    }))
+
 }
 
-function mudaInput(item){
+// if the input has not been filled in it changes the border
+function changeInput(item) {
     item.style.borderColor = "red";
     countErrors++;
 }
